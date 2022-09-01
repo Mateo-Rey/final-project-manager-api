f37cac87b8f26d8d5488df3c530ca92978ce27d9
Mateo-Rey <jrey3darts@gmail.com>
2022-09-01 09:39:40 -0400

    post, get, and put routes are working

--- functions/functions.js
+++ functions/functions.js
@@ -1,46 +1,76 @@
-import {dbConnect} from './dbConnect.js'
-const db = dbConnect()
+import { dbConnect } from "./dbConnect.js";
+
+const db = dbConnect();
 function handleError(err, res) {
-    console.error(err);
-    res.status(500).send(err);
+  console.error(err);
+  res.status(500).send(err);
 }
 
 export const getProjects = (req, res) => {
-    req.params
-    db.collection('projects')
-        .get()
-        .then((collection) => {
-            const projects = collection.docs.map((doc) => doc.data())
-            res.send(projects)
-        })
-        .catch((err) => handleError(err,res))
+  const userId = req.params.userId;
 
-}
+  db.collection("projects")
+    .where("userId", "==", `${userId}`)
+    .get()
+    .then((collection) => {
+      const projects = collection.docs.map((doc) => doc.data());
+      res.send(projects);
+    })
+    .catch((err) => handleError(err, res));
+};
 
 export const addProject = (req, res) => {
-    const newProject = req.body
-    newProject.creationDate = new Date()
-    db.collection('projects')
-        .add(newProject)
-        .then((doc) => {
-            res.status(201).send({
-              success: true,
-              id: doc.id,
-            });
-          })
-          .catch((err) => handleError(err, res));
+  const newProject = req.body;
+  const userId = req.params.userId;
+  newProject.userId = userId;
+  newProject.creationDate = new Date();
+  db.collection("projects")
+    .add(newProject)
+    .then((doc) => {
+      res.status(201).send({
+        success: true,
+        id: doc.id,
+      });
+    })
+    .catch((err) => handleError(err, res));
+};
 
-}
+export const getOneProject = (req, res) => {
+  const userId = req.params.userId;
+  const projectId = req.params.projectId;
+
+  db.collection("projects")
+    .doc(projectId)
+    .get()
+    .then((doc) => {
+      const document = doc.data();
+      console.log(userId, document.userId);
+      if (userId !== document.userId) {
+        res.status(400).send("Incorrect User, Search for a different project.");
+      } else if (userId == document.userId) {
+        res.send(document);
+      }
+    });
+};
 
 export const updateProject = async (req, res) => {
-    const userId = req.params.userId
-    const projectId = req.params.projectId
-    const update = req.body
-    const docRef = db.collection('projects').doc(projectId).set(update, {merge: true})
+  const userId = req.params.userId;
+  const projectId = req.params.projectId;
+  const update = req.body;
+  let document = ''
 
-     await docRef
-        .then(res.status(200).send('Project Updated'))
-        .catch((err)=> {console.log(err)})
-        
-    
-}
+  const docRef = db.collection("projects").doc(projectId);
+
+  await docRef.get().then((doc) => {
+    document = doc.data()
+  })
+  console.log(document)
+  if (document.userId !== userId) {
+    res.status(400).send("Incorrect User, Search for a different project.");
+  } else {
+
+    await docRef.set(update, { merge: true }).then(res.status(200).send("Project Updated")).catch((err) => {
+      console.log(err);
+    });
+  }
+};
--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import functions from 'firebase-functions'
 import cors from 'cors';
-import { addProject, getProjects, updateProject } from './functions.js';
+import { addProject, getProjects, updateProject , getOneProject } from './functions.js';
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.listen(4000, console.log('listening on 4000'))
 
 app.get('/projects/:userId', getProjects)
-app.post('/add-project/:userId/:projectId', addProject)
+app.get('/projects/:userId/:projectId', getOneProject)
+app.post('/add-project/:userId', addProject)
 app.put('/update-project/:userId/:projectId', updateProject)
 export const api = functions.https.onRequest(app)
